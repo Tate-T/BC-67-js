58570686d5ac6085322fd410192afde269eacb1a
Oleksandr <androleks@gmail.com>
2023-12-14 15:38:11 +0300

    14-12-14-34

--- js/module-1-1.js
+++ js/module-1-1.js
@@ -55,7 +55,7 @@
 
 // const productName = "Droid";
 // const pricePerItem = 60000;
-// const message = `You picked ${productName}, 
+// const message = `You picked ${productName},
 // price per item is ${pricePerItem} credits".`
 
 // console.log(message)
@@ -63,13 +63,22 @@
 // Магазин з продажу ремонтних дроїдів готовий до відкриття, залишилося написати скрипт для їх замовлення. Оголоси змінні та присвой їм відповідні значення:
 // pricePerDroid - ціна одного дроїда, значення 800; orderedQuantity - кількість дроїдів у замовленні, значення 6; delivеryFee - вартість доставки, значення 50; totalPrice - загальна сума замовлення до сплати, не забудь про вартість доставки;
 //  - повідомлення про стан замовлення у форматі "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price."
-const pricePerDroid = 800;
-const orderedQuantity = 6;
-const delivеryFee = 50;
-const totalPrice = pricePerDroid * orderedQuantity + delivеryFee;
-console.log(totalPrice);
-const message = `You ordered droids worth ${totalPrice} credits. Delivery (${delivеryFee} credits) is included in total price.`;
-console.log(message);
+//const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const delivеryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + delivеryFee;
+// console.log(totalPrice);
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${delivеryFee} credits) is included in total price.`;
+// console.log(message);
 
-const test = 5 + 5 + "5";
-console.log(test);
+// const test = 5 + 5 + "5";
+// console.log(test);
+
+// Створіть змінну email з вашою електронною адресою.Напишіть скрипт, який перевіряє чи містить змінна email символ @і рахує загальну кількість символів.Результат виведіть в консоль.
+
+const email = 'androleks@gmail.com';
+const checkmail = email.includes('@');
+console.log(checkmail);
+const strlength = email.length;
+console.log(strlength);
+

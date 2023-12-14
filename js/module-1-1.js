337c2ad6e807c20fd749eae51e87c4ca837270c4
Kovtunets Mykola <kovtunets73@gmail.com>
2023-12-14 13:28:47 +0100

    start

---
+++ js/module-1-1.js
@@ -0,0 +1,13 @@
+// Знайти суму перших N натуральних чисел. N запитати у юзера через prompt(while)
+
+// function countSumOfNNumbers() {
+//   const nNumber = Number(prompt("Give an N number"));
+//   let count = 0;
+//   for (let i = 0; count < nNumber; i++) {
+//     count += i;
+//   }
+
+//   return count;
+// }
+
+

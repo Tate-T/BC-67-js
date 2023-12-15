58585532c2c0847d41ccf5633d0a10da763dd964
seheichenko-anna <seheichenko.anna@gmail.com>
2023-12-15 14:48:36 +0200

    Add2

--- js/module-1-2.js
+++ js/module-1-2.js
@@ -28,3 +28,27 @@ function sumNumbers(num1, num2 = 10) {
 const a = 12;
 
 console.log(sumNumbers(a));
+
+// Оголоси дві змінні, які зберігають назву та ціну товару: name і price. Присвоїти змінним наступні характеристики товару (відразу при оголошенні) назва: MacBook Pro 13″, ціна: 10000.
+
+// Використовуючи стандартний рядок, виведи в консоль інформацію про товар. Результат у консолі має виглядати так: 'Обрано «MacBook Pro 13″», ціна за штуку 10000 кредитів'. Присвоїти товару нову ціну - 20000. Використовуючи стандартний рядок, виведи в консоль інформацію про товар. Результат у консолі має виглядати так: 'Обрано «MacBook Pro 13″», ціна за штуку 2000 кредитів'.
+
+function showMessage(productName, productPrice, productQuantity) {
+  const totalPrice = productPrice * productQuantity;
+  const message = `Обрано «${productName}», ціна за штуку ${productPrice} кредитів, загальна вартість складає ${totalPrice}`;
+  return message;
+}
+
+console.log(showMessage("MacBook Pro 13″", 10000, 5));
+console.log(showMessage("MacBook R″", 20000, 90));
+
+// function showMessage(productName, productPrice, productQuantity) {
+//   return `Обрано «${productName}», ціна за штуку ${productPrice} кредитів, загальна вартість складає ${
+//     productPrice * productQuantity
+//   }`;
+// }
+
+// const showMessage = (productName, productPrice, productQuantity) =>
+//   `Обрано «${productName}», ціна за штуку ${productPrice} кредитів, загальна вартість складає ${
+//     productPrice * productQuantity
+//   }`;

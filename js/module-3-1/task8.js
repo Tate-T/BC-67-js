2f47001b884d209cbc6388c5fef9274476fe2a74
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2023-12-20 15:29:33 +0100

    task-8

--- js/module-3-1/task8.js
+++ js/module-3-1/task8.js
@@ -1,13 +1,12 @@
 /*
 Створіть масив фруктів, для якого:
 1) за допомогою методів масиву видаліть по фрукту з початку та кінця,  на їх місце додати нові фрукти;
-2) видалити 3-й елемент, та на його місце вставити декілька інших фруктів використовуючи метод (splice);
-3) із зміненого масиву створіть повідомлення.
+
 */
 
-const fruits = ["apple", "banana", "orange"];
+// const fruits = ["apple", "banana", "orange"];
 //console.log(`Start array: ${fruits}`);
-console.log(fruits);
+// console.log(fruits);
 
 // First solution
 
@@ -27,3 +26,14 @@ console.log(fruits);
 // console.log(fruits);
 // fruits.splice(fruits.length - 1, 1, "avocado");
 // console.log(fruits);
+
+// 2) видалити 3-й елемент, та на його місце вставити декілька інших фруктів використовуючи метод (splice);
+// const fruits = ["apple", "banana", "orange"];
+// fruits.splice(2, 1, "nuts", "avocado");
+// console.log(fruits);
+// // 3) із зміненого масиву створіть повідомлення.
+// let message = "У кошику знаходяться фрукти:";
+// for (const fruit of fruits) {
+//   message = message + fruit + ", ";
+// }
+// console.log(message);

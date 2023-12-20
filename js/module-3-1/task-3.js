7add989069c14958760d5a74c32b91dcac838d4e
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2023-12-20 14:22:17 +0100

    task 3 morozova

---
+++ js/module-3-1/task-3.js
@@ -0,0 +1,11 @@
+// // Створити скрипт який поверне суму всіх чисел в масиві
+// const numbers = [2, 4, 8, 10];
+// let totalNumber = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+//     // totalNumber = totalNumber + number;
+//     totalNumber += number;
+//     console.log(totalNumber);
+    
+// }
+// console.log(totalNumber);
---
+++ js/module-3-1/task-4.js
@@ -0,0 +1,9 @@
+// Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
+const numbers = [5, 8, 10, 9, 6];
+// for(const number of numbers){
+//     console.log(number);
+// }
+for(let i = 0; i < numbers.length; i++){   
+    // начать с нуля добавляй по одному пока i меньше длинны масива
+    console.log(numbers[i]);
+}

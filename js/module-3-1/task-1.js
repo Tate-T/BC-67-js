9ef1f1ff6eedc71a41b024138c61d58663162d8f
Andrii2024 <krovlya2005@gmail.com>
2023-12-20 14:49:30 +0200

    task-1-2 module3.1

---
+++ js/module-3-1/task-1.js
@@ -0,0 +1,11 @@
+// Створити масив з трьох чисел.
+// Змінити значення другого елемента масиву на 10.
+const numbers = [];
+numbers[0] = 1;
+numbers[1] = 2;
+numbers[2] = 3;
+const firstNum = numbers[0];
+
+console.log(numbers);
+numbers[1] = 10;
+console.log(numbers);
---
+++ js/module-3-1/task-2.js
@@ -0,0 +1,11 @@
+// Створити масив із трьох рядків. Додати до масиву ще один рядок.
+const frends = ["Aleks", "Marco", "Polo"];
+frends[3] = "Fren";
+const piople = frends.length;
+frends[piople] = "Tetiana";
+console.log(frends);
+frends.push("Volodimir");
+console.log(frends);
+frends.unshift("Olia");
+console.log(frends);
+console.log(Array());

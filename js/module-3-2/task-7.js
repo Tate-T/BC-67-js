1549dfa925edc88643e2ed4ada1929decee2c6d3
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2023-12-22 16:24:02 +0100

    task7

---
+++ js/module-3-2/task-7.js
@@ -0,0 +1,38 @@
+// Знайти всі елементи масиву, що містять задану підстроку,
+// та об'єднати їх у новий масив, після чого вивести у консоль лог повідомлення "Масив елементів,
+//  що містять підстроку substring: ..." .Результат повинен бути розділений комами та пробілами.(використати indexOf)
+// const arr = ["JavaScript", "HTML", "CSS", "React"];
+// const str = "java";
+// const normalizeArr = arr.join(", ").toLowerCase().split(", ");
+// const newArr = [];
+// let message = `Масив елементів, що містять підстроку substring: `;
+// for (let i = 0; i < normalizeArr.length; i++) {
+//   if (normalizeArr[i].includes(str.toLowerCase())) {
+//     newArr.push(normalizeArr[i]);
+//     message = message + `${normalizeArr[i]}, `;
+//     console.log(message);
+//   }
+// }
+// console.log(message);
+
+// const arr = ["JavaScript", "HTML", "CSS", "React"];
+// const str = "java";
+// const normalizeArr = arr.join(", ").toLowerCase().split(", ");
+
+// console.log(normalizeArr);
+// // const newArr = [];
+// let message = `Масив елементів, що містять підстроку substring: `;
+
+// for (let i = 0; i < normalizeArr.length; i++) {
+//   if (normalizeArr[i].includes(str.toLowerCase())) {
+//     const index = normalizeArr.indexOf(normalizeArr[i]);
+//     console.log(index);
+
+//     const word = arr.slice(index, index + 1);
+//     console.log(word);
+//     message = message + `${word}`;
+//     console.log(message);
+//   }
+// }
+
+// console.log(message);

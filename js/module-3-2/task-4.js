8bc22dabf9a84ce0a2497c10d4cc16229477209f
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2023-12-22 14:08:07 +0100

    task4

---
+++ js/module-3-2/task-4.js
@@ -0,0 +1,27 @@
+// Створіть масив styles з елементами 'Джаз' i 'Блюз'.
+//     Додайте 'Рок-н-ролл' в кінець масиву.Замініть значення в середині масиву на 'Класика'.
+//
+
+const array = ["Джаз", "Блюз"];
+// // array.push("Рок-н-ролл");
+// // console.log(array);
+// // array.splice(array.length - 1, 0, "Рок-н-ролл");
+// // console.log(array);
+// array[2] = "Рок-н-ролл";
+// console.log(array);
+// array.splice(array.length - 2, 1, "Класика");
+// console.log(array);
+
+//  Видаліть перший елемент масиву і покажіть його.
+// array.shift();
+// array.splice(0, 1);
+
+// console.log(array);
+// //     Додайте 'Реп' і 'Реггі' на початок масиву.Виведіть масив в консоль.
+// // array.unshift("Реп", "Реггі");
+// // console.log(array);
+// array.splice(0, 0, "Реп", "Реггі");
+// console.log(array);
+// ===============================
+// array.splice(0, 1, "Реп", "Реггі");
+// console.log(array);

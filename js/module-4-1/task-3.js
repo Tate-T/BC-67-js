7cfb97ca0c52787d74a486e08a87a962156c759d
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2023-12-26 14:40:23 +0100

    task-3

---
+++ js/module-4-1/task-3.js
@@ -0,0 +1,22 @@
+// Створіть об'єкт "bankAccount" з властивостями "ownerName", "accountNumber", "balance".
+//  Додайте до об'єкту метод "deposit", який дозволяє додавати гроші на рахунок,
+//  та метод "withdraw", який дозволяє знімати гроші з рахунку.
+//  Методи повинні спрацьовувати при натисканні на кнопки “поповнити рахунок”
+// та отримати ‘готівку’ відповідно.Після проведення операції виводити повідомлення про залишок на рахунку.
+
+// const bankAccount = {
+//   ownerName: "Volodymyr",
+//   accountNumber: "123456",
+//   balance: 9999,
+//   deposit(sum) {
+//     this.balance += sum;
+//     return this.balance;
+//   },
+//   withdraw(sumBeack) {
+//     this.balance -= sumBeack;
+//     return `знято ${sumBeack}, Ваш баланс складає ${this.balance}`;
+//   },
+// };
+
+// console.log(bankAccount.deposit(5000));
+// console.log(bankAccount.withdraw(3000));

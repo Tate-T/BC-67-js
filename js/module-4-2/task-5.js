0d1fa3a919ea651d8fba32d48e196729826c2af9
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2023-12-27 14:32:32 +0100

    task-5

---
+++ js/module-4-2/task-5.js
@@ -0,0 +1,29 @@
+// // Створіть масив об'єктів "книг" з властивостями "назва", "автор" та "рік видання". Напишіть функцію "findBooksByAuthor",
+// //яка буде повертати масив книг відповідного автора.
+// const books = [
+//   { title: "Війна і мир", author: "Лев Толстой", year: 1869 },
+//   {
+//     title: "Преступление и наказание",
+//     author: "Федор Достоевский",
+//     year: 1866,
+//   },
+//   {
+//     title: "Гаррі Поттер і філософський камінь",
+//     author: "Джоан Роулінг",
+//     year: 1997,
+//   },
+// //   { title: "Мастер и Маргарита", author: "Михаил Булгаков", year: 1967 },
+//   { title: "1984", author: "Джордж Оруелл", year: 1949 },
+//   { title: "test book", author: "Михаил Булгаков", year: 1987 },
+// ];
+// const input = prompt("vvedit` nazvy");
+// function findBooksByAuthor(input) {
+//   const arr = [];
+//   for (const book of books) {
+//     if (book.author === input) {
+//       arr.push(book);
+//     }
+//   }
+//   return arr;
+// }
+// console.log(findBooksByAuthor(input));

32753b20cd7300be71068caf7b059e18cdd9843e
Kateryna02 <150830031+Kateryna02@users.noreply.github.com>
2023-12-28 14:12:36 +0100

    task 2

---
+++ js/module-5-1/task-2.js
@@ -0,0 +1,26 @@
+// инлайновы колбеки
+
+// const numbers = [5, 7, 35, 85];
+
+// function multiNums(numbers, callback, makeAlert) {
+//     let total = 1;
+//     numbers.forEach((number) => {
+//         total *= number;
+//     });
+//     const message = callback(total);
+//     const testMessange = makeAlert(message);
+
+//     return testMessange;
+
+// }
+
+
+
+// console.log(multiNums(numbers, (result) => `Загальна сума добутку складає ${result}`, (message) => {
+//     const name = prompt(`введить имя`);
+//     const newMassage = `витаемо ${name}` + message;
+//     return newMassage;
+// }));
+
+
+

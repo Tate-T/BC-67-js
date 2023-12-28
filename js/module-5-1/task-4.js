351cdde07e4ec58513e6b7ba3b9d6eb21d41b0f8
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2023-12-28 14:59:44 +0100

    task-4

---
+++ js/module-5-1/task-4.js
@@ -0,0 +1,21 @@
+// const students = [
+//   { name: "Андрій", surname: "Іванов", grade: 4.5 },
+//   { name: "Олександр", surname: "Петров", grade: 3.9 },
+//   { name: "Марія", surname: "Сидорова", grade: 4.8 },
+//   { name: "Ірина", surname: "Федорова", grade: 4.2 },
+// ];
+
+// // znaitu seredniy bal ychniv
+
+// function countGrade(students) {
+//   const grades = students.map((student) => student.grade);
+//   console.log(grades);
+//   let totalgrades = 0;
+//   grades.forEach((grade) => {
+//     totalgrades += grade;
+//   });
+//   const midle = totalgrades / grades.length;
+//   return midle;
+// }
+
+// console.log(countGrade(students));

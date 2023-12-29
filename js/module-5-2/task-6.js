a9294eab8e6de223b1761366da02da51a6b8b5ef
Normal One <149543061+Fedir-M@users.noreply.github.com>
2023-12-29 09:28:56 -0500

    evry & some methods

---
+++ js/module-5-2/task-6.js
@@ -0,0 +1,31 @@
+//TODO Метод Array.prototype.every() поелементо перебирає оригінальний массив,
+//TODO вертає true якщо всі елементи масиву задовільняють умову
+
+// const teams = ["Chealsy", "Liverpool", "Dynamo", "Barcelona", "Real"];
+
+// const isRhere = teams.every((team) => team.includes("r"));
+// console.log(isRhere)
+
+
+//TODO Метод Array.prototype.some() поелементо перебирає оригінальний массив та вертає true якщо хоча б один елемент масиву задовільняє умову
+
+// const teams = ["Chealsy", "Liverpool", "Dynamo", "Barcelona", "Real"];
+
+// const isRhere = teams.some((team) => team.includes("r"));
+// console.log(isRhere)
+// =====================
+
+//TODO Have all users age between 17 and 40 years?
+
+// const people = [
+//   { name: "John", age: 32, occupation: "programmer" },
+
+//   { name: "Jane", age: 26, occupation: "teacher" },
+
+//   { name: "Mike", age: 42, occupation: "engineer" },
+
+//   { name: "Emily", age: 17, occupation: "designer" },
+// ];
+
+// const selectedAge = people.every((person) => person.age >= 17 && person.age <= 50);
+// console.log(selectedAge);

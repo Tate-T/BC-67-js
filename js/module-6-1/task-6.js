692646c30cce207684f5e00a951e4fd885181fd1
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-08 14:27:33 +0100

    t6-6

---
+++ js/module-6-1/task-6.js
@@ -0,0 +1,14 @@
+// Створіть функцію - конструктор Rectangle,
+//     що містить властивості width та height.
+//     Додайте метод getArea(), який повертає площу прямокутника.
+
+// function Rectangle(width, height) {
+//   this.width = width;
+//   this.height = height;
+//   this.getArea = function () {
+//     return width * height;
+//   };
+// }
+
+// const rectangle1 = new Rectangle(10, 5);
+// console.log(rectangle1.getArea()); // 50

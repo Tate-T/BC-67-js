430025c932b0b1402dae4fd84ca4bcffc9b8cb7d
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-15 14:13:05 +0100

    t2

---
+++ js/module-8-1/task-2.js
@@ -0,0 +1,17 @@
+// Напиши скрипт в якому є три радіобатони зі значенями кольрів.
+// При обиранні на будь якого колір фону боді змінюється відповідно
+
+// const formEl = document.querySelector("form");
+// console.log(formEl);
+
+// function onChangeInput(e) {
+//   if (e.target.value === "red") {
+//     document.body.style.backgroundColor = "red";
+//   } else if (e.target.value === "white") {
+//     document.body.style.backgroundColor = "white";
+//   } else if (e.target.value === "green") {
+//     document.body.style.backgroundColor = "green";
+//   }
+// }
+
+// formEl.addEventListener("change", onChangeInput);

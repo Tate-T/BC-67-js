2b23dd3b9013831b4d144d200bb1b372a282aca0
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-15 13:59:45 +0100

    t1

---
+++ js/module-8-1/task-1.js
@@ -0,0 +1,26 @@
+// // Перемістити елемент з id = "box" наліво, якщо користувач натискає
+// //  стрілку вліво,
+// //     та направо, якщо користувач натискає стрілку вправо,
+// // під час події keydown.
+
+// const boxEl = document.querySelector(".box");
+// // console.log(boxEl);
+// const keyCodeLeft = "KeyA";
+// const keyCodeRight = "KeyM";
+// function onChangeBox(e) {
+//   if (e.code === keyCodeLeft) {
+//     boxEl.classList.add("box1");
+//     boxEl.classList.replace("box2", "box1");
+//   } else if (e.code === keyCodeRight) {
+//     boxEl.classList.replace("box1", "box2");
+//   }
+// }
+// document.addEventListener("keydown", onChangeBox);
+
+// // document.addEventListener("keydown", (e) => {
+// //   e.preventDefault();
+// //   console.log(e.key);
+// //   //   if (e.code === "KeyA") {
+// //   //     console.log(test);
+// //   //   }
+// // });

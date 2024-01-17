7b3d453ee99e6984f4f5788cfe49d45de990dbc9
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-01-17 14:13:19 +0200

    task1

---
+++ js/module-9-1/task-1.js
@@ -0,0 +1,13 @@
+// Створіть функцію, яка буде додавати об'єкт в localStorage. Назва об'єкту має бути ключем, а сам об'єкт - значенням.
+
+function addToLS(user) {
+    const 
+    localStorage.setItem()
+}
+
+
+const user = {
+    name: 'Alina',
+    hoby: 'box',
+    age: 25
+}

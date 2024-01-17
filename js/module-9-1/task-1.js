b322482ac8f368c6afb6086cde2131101f7f6556
Yevhen01 <148199909+Yevhen01@users.noreply.github.com>
2024-01-17 14:42:45 +0200

    localstorage JSON methods

--- js/module-9-1/task-1.js
+++ js/module-9-1/task-1.js
@@ -1,13 +1,47 @@
-// Створіть функцію, яка буде додавати об'єкт в localStorage. Назва об'єкту має бути ключем, а сам об'єкт - значенням.
+// // Створіть функцію, яка буде додавати об'єкт в localStorage. Назва об'єкту має бути ключем, а сам об'єкт - значенням.
+// function addToLocalStorage(user1, user2) {
+//   const user1data = JSON.stringify(user1);
+//   const user2data = JSON.stringify(user2);
 
-function addToLS(user) {
-    const 
-    localStorage.setItem()
-}
+//   localStorage.setItem("obj1", user1data);
+//   localStorage.setItem("obj2", user2data);
 
+//   console.log(user1data);
+//   console.log(user2data);
+// }
 
-const user = {
-    name: 'Alina',
-    hoby: 'box',
-    age: 25
-}
+// const user1 = {
+//   firstName: "Vasya",
+//   age: 20,
+// };
+
+// const user2 = {
+//   firstName: "Alex",
+//   age: 30,
+// };
+
+// addToLocalStorage(user1, user2);
+
+// // Створіть функцію, яка буде отримувати об'єкт з localStorage. Назва об'єкту має бути ключем, а сам об'єкт - значенням.
+// function getDataFromLocalStorage(obj1) {
+//   const data = localStorage.getItem(obj1);
+//   const newData = JSON.parse(data);
+
+//   return newData;
+// }
+
+// console.log(getDataFromLocalStorage("obj1"));
+
+// // Створіть функцію, яка буде видаляти значення з localStorage за ключем.
+// function removeDataFromLocalStorage(obj1) {
+//   localStorage.removeItem(obj1);
+// }
+
+// removeDataFromLocalStorage("obj1");
+
+// // Створіть функцію, яка буде видаляти всі значення з localStorage.
+// function removeAllData() {
+//   localStorage.clear();
+// }
+
+// removeAllData();

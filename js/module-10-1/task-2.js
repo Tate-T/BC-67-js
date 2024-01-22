a14772ad63368dbc0befe71266b00c74e0fb725c
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-22 13:50:41 +0100

    task -1-2-3

---
+++ js/module-10-1/task-2.js
@@ -0,0 +1,35 @@
+// Завдання "Лічильник": Створіть лічильник,
+// який збільшується на одиницю кожну секунду.
+//  Виводьте значення лічильника на сторінці.
+
+// const boxEl = document.querySelector(".box");
+// console.log(boxEl);
+
+// class Time {
+//   constructor() {}
+// }
+
+// let countSec = 0;
+
+// function counter() {
+//   countSec += 1;
+//   boxEl.innerHTML = `${countSec}`;
+// }
+
+// setInterval(counter, 1000);
+
+// ===================================
+
+// Завдання "Зміна кольору": Створіть блок,
+//     який змінює свій фоновий колір кожні 3 секунди.
+//     Використовуйте setInterval для зміни кольору.
+
+// const boxEl = document.querySelector(".box");
+
+// setInterval(changeColor, 3000);
+// function changeColor() {
+//   const red = Math.random() * (255 - 0) + 0;
+//   const green = Math.random() * (255 - 0) + 0;
+//   const blue = Math.random() * (255 - 0) + 0;
+//   boxEl.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// }
---
+++ js/module-10-1/task-3.js
@@ -0,0 +1,16 @@
+// Завдання "Затримка повідомлення": Створіть функцію,
+//     яка приймає текстове повідомлення та затримує його
+//      виведення на сторінку на певний час,
+//     використовуючи setTimeout.
+
+// const pElem = document.querySelector("p");
+
+// function callMessage() {
+//   const message = "Затримка повідомлення";
+//   setTimeout(showMessage, 4000, message);
+// }
+// function showMessage(message) {
+//   pElem.innerHTML = `${message}`;
+// }
+
+// callMessage();
